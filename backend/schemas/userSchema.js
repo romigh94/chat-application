2c4271eefb5b0cb166179cc0a26fc54e2f638a56
Romina Ghaderi <rominaa.ghaderi94@gmail.com>
2023-09-19 22:04:46 +0200

    lägger in så man kan välja avatar

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -4,7 +4,9 @@ const userSchema = new mongoose.Schema({
     username: String,
     email: String,
     password: String,
-    confirmPassword: String
+    confirmPassword: String,
+    isAvatarImageSet: Boolean,
+    avatarImage: String
 })
 
 module.exports = mongoose.model('Users', userSchema)
--- src/components/SetAvatar.vue
+++ src/components/SetAvatar.vue
@@ -1,20 +1,32 @@
 <template>
+
+<!-- Find one gif loader because the images are slow to load -->
   <div class="avatar-container">
     <div class="title-container">
       <h1>Pick an avatar as your profile pic</h1>
     </div>
 
+    <div class="error-container">
+      {{errormsg}}
+    </div>
+
     <div class="avatars">
-      <div v-for="(avatar, index) in avatars" :key="index" class="avatar-item">
-        <img :src="'data:image/svg+xml;base64' + avatar" alt="Avatar" @click="handleProfilePic(index)" />
+      <div v-for="(avatar, index) in avatars" :key="index" class="avatar">
+        <div :class="{ selected: index === selectedAvatar }">
+        <img :src="'data:image/svg+xml;base64,' + avatar" alt="Avatar" @click="handleProfilePic(index)" />
+        </div>
       </div>
     </div>
+
+    <button @click="setProfilePicture" class="profilepic-submit-btn" >Set as Profile Picture</button>
+
   </div>
 </template>
 
 <script>
 const api = "https://api.multiavatar.com/45678945";
 import axios from "axios";
+import {Buffer} from 'buffer'
 
 export default {
   data() {
@@ -22,19 +34,25 @@ export default {
       avatars: [],
       isLoading: true,
       selectedAvatar: undefined,
+      errormsg: ''
     };
   },
   methods: {
     async handleProfilePic(index) {
-      this.selectedAvatar = this.avatars[index];
-      // Perform any other actions you want based on the selected avatar
+      this.selectedAvatar = index
     },
+    async setProfilePicture() {
+      if (this.selectedAvatar === undefined) {
+        this.errormsg = "Please select an avatar"
+      } 
+    }
   },
   async mounted() {
     const data = [];
     for (let i = 0; i < 4; i++) {
       const response = await axios.get(`${api}/${Math.round(Math.random() * 1000)}`)
-      data.push(response.data);
+      const buffer = new Buffer(response.data)
+      data.push(buffer.toString("base64"))
     }
     this.avatars = data;
     this.isLoading = false;
